63177c6bbf36f85ac5d9e3b10e24ec18d0584283
Christopher Cowan <chrisjoelcowan@gmail.com>
2018-08-13 23:59:26 -0400

    code cleanup

--- Crisp/Repl.cs
+++ Crisp/Repl.cs
@@ -7,62 +7,89 @@ namespace Crisp
 {
     class Repl
     {
+        enum Commands
+        {
+            EvalCode,
+            Unknown,
+            Quit,
+            WriteEnvironment,
+            WriteSymbolTable,
+        }
+
         public static void Run(TextReader reader, TextWriter writer)
         {
             var environment = new Runtime.Environment();
             var parser = new Parser();
 
-            var quit = false;
-            while (!quit)
+            while (true)
             {
-                try
+                var input = Prompt(reader, writer);
+                var command = ParseInput(input);
+                switch (command)
                 {
-                    writer.Write("> ");
-                    var code = reader.ReadLine();
-                    if (code.Length > 0 && code[0] == ':')
-                    {
-                        quit = ExecuteCommand(code, parser, environment, writer);
-                    }
-                    else
-                    {
-                        EvalAndPrint(code, parser, environment, writer);
-                    }
-                }
-                catch (CrispException e)
-                {
-                    writer.WriteLine(e.FormattedMessage());
+                    case Commands.EvalCode:
+                        Evaluate(input, parser, environment, writer);
+                        break;
+
+                    case Commands.Unknown:
+                        writer.WriteLine("error: unknown command");
+                        break;
+
+                    case Commands.Quit:
+                        goto QuitRepl;
+
+                    case Commands.WriteEnvironment:
+                        environment.Write();
+                        break;
+
+                    case Commands.WriteSymbolTable:
+                        parser.SymbolTable.Write();
+                        break;
                 }
             }
+
+            QuitRepl: writer.WriteLine("goodbye");
         }
 
-        private static bool ExecuteCommand(string code, Parser parser, Runtime.Environment environment, TextWriter writer)
+        static string Prompt(TextReader reader, TextWriter writer)
         {
-            var args = code.Split(new[] { ' ' },
-                StringSplitOptions.RemoveEmptyEntries);
+            writer.Write("> ");
+            return reader.ReadLine();
+        }
+
+        static Commands ParseInput(string input)
+        {
+            if (!input.StartsWith(":"))
+            {
+                return Commands.EvalCode;
+            }
+
+            var args = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             switch (args[0])
             {
                 case ":q":
-                    return true;
+                    return Commands.Quit;
                 case ":e":
-                    environment.Write();
-                    break;
+                    return Commands.WriteEnvironment;
                 case ":st":
-                    parser.SymbolTable.Write();
-                    break;
+                    return Commands.WriteSymbolTable;
                 default:
-                    writer.WriteLine($"Unknown command <{code}>");
-                    break;
+                    return Commands.Unknown;
             }
-
-            return false;
         }
 
-        private static void EvalAndPrint(string code, Parser parser,
-            Runtime.Environment environment, TextWriter writer)
+        static void Evaluate(string code, Parser parser, Runtime.Environment environment, TextWriter writer)
         {
-            var program = parser.Parse(code);
-            var result = Evaluator.Run(program, environment);
-            writer.WriteLine(result);
+            try
+            {
+                var program = parser.Parse(code);
+                var result = Evaluator.Run(program, environment);
+                writer.WriteLine(result);
+            }
+            catch (CrispException e)
+            {
+                writer.WriteLine(e.FormattedMessage());
+            }
         }
     }
 }
